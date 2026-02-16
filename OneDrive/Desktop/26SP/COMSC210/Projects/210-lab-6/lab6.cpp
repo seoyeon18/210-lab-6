5dba70b5a9e9c1caad5ec7da4e4a79d892dec9c3
Bright <brightsy04@gmail.com>
2026-02-15 20:12:28 -0800

    lab-6-3

--- OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-6/lab6.cpp
+++ OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-6/lab6.cpp
@@ -7,15 +7,38 @@ double sumArray(double* arr, int size);
 
 int main()
 {
-    
+    const int SIZE = 5;
+    double* arr = new double[SIZE];
+
+    enterArrayData(arr, SIZE);
+    outputArrayData(arr, SIZE);
+
+    double total = sumArray(arr, SIZE);
+    cout << "Sum of values: " << total << endl;
+
+    delete[] arr;
+
+    return 0;
 }
 
 void enterArrayData(double* arr, int size)
 {
+    cout << "Data Element for array:\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Element" << i << ": ";
+        cin >> *(arr + i);
+    }
 }
 
 void outputArrayData(double* arr, int size)
 {
+    cout << "Output array elements: ";
+    for (int i = 0 ; i < size; i++)
+    {
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
     
 }
 
