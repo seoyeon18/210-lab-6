c50fb2c3e83256a6992191c946436a5fdaaecb75
Bright <brightsy04@gmail.com>
2026-02-15 20:04:12 -0800

    lab-6-2

---
+++ OneDrive/Desktop/26SP/COMSC210/Projects/210-lab-6/lab6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void enterArrayData(double* arr, int size);
+void outputArrayData(double* arr, int size);
+double sumArray(double* arr, int size);
+
+int main()
+{
+    
+}
+
+void enterArrayData(double* arr, int size)
+{
+}
+
+void outputArrayData(double* arr, int size)
+{
+    
+}
+
+double sumArray(double* arr, int size)
+{
+   
+}
